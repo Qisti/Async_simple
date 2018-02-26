60f622b15f7f37158422e372adb8305e7fca6d39
Qisti <36026889+Qisti@users.noreply.github.com>
2018-02-26 14:02:53 +0700

    Create checkFile.js

---
+++ checkFile.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.on('line', (line) => {
+	console.log(`File or folder name: ${line}`);
+
+	fs.open(line, 'r', (err, fd) => {
+		if (err) {
+			if (err.code === 'ENOENT') {
+				console.error('file does not exist');
+				return;
+			}
+		} else {
+			console.log("file exist");
+		} 
+	});
+
+	rl.close();
+});
+
+console.log("input folder name or file name");
